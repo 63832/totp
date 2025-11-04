a9b1761b350fde3c0f015b61bef31e036de2816b
63832 <63832@gymnasium.ax>
2025-11-04 13:58:42 +0200

    Lagt till registreringformulär och klasser för användarhantering

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// Registrera användare 
+$router->get('/register', 'UserController@showRegister');
+$router->post('/register', 'UserController@register');
