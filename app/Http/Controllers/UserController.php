6a27d9654c9cf46836f026f2ee0ffcc05ae2ca7b
63832 <63832@gymnasium.ax>
2025-11-04 15:47:14 +0200

    Mail-svar med qr kod för inloggning visas efter registrering

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,23 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Services\TotpQrService;
 use App\Storage\UserRepository;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
-    public function __construct(private UserRepository $repo) {
+    public function __construct(private UserRepository $repo)
+    {
 
     }
 
-    public function showRegister() {
+    public function showRegister()
+    {
         return View::make('register');
     }
 
-    public function register(Request $request) {
-        $user = User::factory()->make($request->request->all());
+    public function register(Request $request)
+    {
+        try {
+            $user = User::factory()->make($request->request->all());
 
-        $this->repo->add($user);
+            $this->repo->add($user);
+
+            $qr = TotpQrService::generateQrCode($user);
+            return View::make('mail', ['qr'=>$qr]);
+            
+        } catch (UniqueConstraintViolationException $e) {
+            return View::make('register', ['message'=>'User email already exists.']);
+        }
     }
 }
